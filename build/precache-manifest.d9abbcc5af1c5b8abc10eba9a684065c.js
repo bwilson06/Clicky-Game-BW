41d814842de220b6d53757c5b960f576e707e046
Bradley Wilson <bluedirtbike6@gmail.com>
2020-08-10 22:56:02 -0500

    fix heroku error

---
+++ build/precache-manifest.d9abbcc5af1c5b8abc10eba9a684065c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2010a1aab43277811814da776a1b1ead",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4bacc89d1fcde0508a4d",
+    "url": "/static/css/main.1bb80a7b.chunk.css"
+  },
+  {
+    "revision": "9b37f7eae4ac3739e8fc",
+    "url": "/static/js/2.d8301940.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d8301940.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bacc89d1fcde0508a4d",
+    "url": "/static/js/main.73c8a614.chunk.js"
+  },
+  {
+    "revision": "42d008684aaba4b4ae2e",
+    "url": "/static/js/runtime-main.4cc0c8fb.js"
+  }
+]);
